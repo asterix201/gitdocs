bd0c9b477a3ef52e39c3915bc2fa7eaa5efa4156
KHRAMOV GENNADIY <gennadiy.khramov@decathlon.com>
2020-08-11 23:42:09 +0300

    before deleting to start new app

--- app/main/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class AddForm(FlaskForm):
-    project = StringField('Git project name', validators=[DataRequired()])
-    submit = SubmitField('Register')
